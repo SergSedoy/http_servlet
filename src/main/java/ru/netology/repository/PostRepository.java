7a444a7cbed8673fc96cb8aed298597297e3203b
SergSedoy <SergeySedoy2002@yandex.ru>
2021-07-16 00:36:43 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,39 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+
+// Stub
+public class PostRepository {
+    private static List<Post> posts = new ArrayList<>();
+    private static long id;
+
+    public List<Post> all() {
+        return posts;
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.empty();
+    }
+
+    public synchronized Post save(Post post) {
+        if (post.getId() == 0) {
+            long newId = id++;
+            post.setId(newId);
+            posts.set((int) newId, post);
+        }
+        if (post.getId() != 0) {
+            try {
+                post = posts.get((int) post.getId());
+            } catch (Exception e) {
+                post.setId(0);
+                return save(post);
+            }
+        }
+        return post;
+    }
+
+    public synchronized void removeById(long id) {
+    }
+}
