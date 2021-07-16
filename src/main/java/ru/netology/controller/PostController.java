da8bfddd4fbaac8d1384474c89ee0f9c6712df2b
SergSedoy <78666743+SergSedoy@users.noreply.github.com>
2021-07-16 15:56:29 +0300

    Update PostController.java
    revision

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -24,7 +24,10 @@ public class PostController {
     }
 
     public void getById(long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+        response.setContentType(APPLICATION_JSON);
+        Post post = service.getById(id);
+        final var gson = new Gson();
+        response.getWriter().print(gson.toJson(post));
     }
 
     public void save(Reader body, HttpServletResponse response) throws IOException {
@@ -36,6 +39,6 @@ public class PostController {
     }
 
     public void removeById(long id, HttpServletResponse response) {
-        // TODO: deserialize request & serialize response
+        service.removeById(id);
     }
 }
